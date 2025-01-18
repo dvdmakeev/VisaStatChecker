1192386851e8eea9210a853dc684750a1918f788
David Makeev <dvdmakeev@gmail.com>
2025-01-18 20:12:27 +0000

    test change

--- src/bot.py
+++ src/bot.py
@@ -10,7 +10,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing ?")
+    bot.reply_to(message, "Howdy, how are you doing?")
 
 
 @bot.message_handler(func=lambda msg: True)
