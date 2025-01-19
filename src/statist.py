7fc0a184062dc1ee4f17ebb70bea1edeff6771cb
David Makeev <dvdmakeev@gmail.com>
2025-01-19 17:26:03 +0000

    enum used, header values fixed

--- src/statist.py
+++ src/statist.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from enum import Enum
 import pandas as pd
 
 from googleapiclient.errors import HttpError
@@ -18,29 +19,37 @@ def fetch_data_from_sheets(sheet_id, range_name):
             return pd.DataFrame()
         
         headers = values[0]
+
+        headers = [header.split("\n")[0] for header in headers]
+
         data = values[1:]
         return pd.DataFrame(data, columns=headers)
     
     except HttpError as err:
         print(f"An error occurred: {err}")
         return pd.DataFrame()
+    
+
+class TimePeriod(Enum):
+    WEEK = "week"
+    MONTH = "month"
+    THREE_MONTH = "3Month"
+    SIX_MONTH = "6Month"
 
 
 def filter_data(df, time_period, only_first_visa=True, only_touristic_visa=True):
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
 
+    end_date = datetime.now()
+
     # Time period filtering
-    if time_period == "week":
-        end_date = datetime.now()
+    if time_period == TimePeriod.WEEK.value:
         start_date = end_date - pd.DateOffset(weeks=1)
-    elif time_period == "month":
-        end_date = datetime.now()
+    elif time_period == TimePeriod.MONTH.value:
         start_date = end_date - pd.DateOffset(months=1)
-    elif time_period == "3Month":
-        end_date = datetime.now()
+    elif time_period == TimePeriod.THREE_MONTH.value:
         start_date = end_date - pd.DateOffset(months=3)
-    elif time_period == "6Month":
-        end_date = datetime.now()
+    elif time_period == TimePeriod.SIX_MONTH.value:
         start_date = end_date - pd.DateOffset(months=6)
     else:
         raise ValueError("Invalid time period. Choose from: week, month, 3Month, 6Month.")
@@ -48,10 +57,10 @@ def filter_data(df, time_period, only_first_visa=True, only_touristic_visa=True)
     df_filtered = df[(df['Timestamp'] >= start_date) & (df['Timestamp'] <= end_date)]
 
     if only_first_visa:
-        df_filtered = df_filtered[df_filtered['Предыдущая визовая история'].str.contains("первая", na=False)]
+        df_filtered = df_filtered[df_filtered['Предыдущая визовая история.'].str.contains("первая", na=False)]
 
     if only_touristic_visa:
-        df_filtered = df_filtered[df_filtered['Тип визы'] == 'Туристическая']
+        df_filtered = df_filtered[df_filtered['Тип визы.'] == 'Туристическая']
     
     return df_filtered
 
@@ -67,8 +76,8 @@ def calculate_top_countries(df):
 
     country_visa_counts['rate'] = country_visa_counts['Multi_visa_decisions'] / country_visa_counts['One_entry_visa_decisions']
     
-    # Sort by the rate in descending order and return the top 3 countries
-    top_countries = country_visa_counts.sort_values('rate', ascending=False).head(3)
+    # Sort by the rate in descending order and return the top 5 countries
+    top_countries = country_visa_counts.sort_values('rate', ascending=False).head(5)
     
     return top_countries
 
@@ -85,10 +94,12 @@ def get_top_visa_countries(time_period, only_first_visa=True, only_touristic_vis
 
 
 def main():
-    top_countries = get_top_visa_countries("week", only_first_visa=True, only_touristic_visa=True)
+    top_countries = get_top_visa_countries("6Month", only_first_visa=True, only_touristic_visa=True)
     
+    print(len(top_countries))
     print("Data fetched from Google Sheets:")
     print(top_countries)
+
 
 if __name__ == "__main__":
     main()
